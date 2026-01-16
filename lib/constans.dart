9fe0136ab07eda0032cbe4f25c62baf16db93930
Ahmed Zahran <ahmedzhran82@gmail.com>
2026-01-16 10:32:08 +0200

    splash view ui

---
+++ lib/constans.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF100B20);
+const kLogo = 'assets/images/Logo.png';
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/constans.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
@@ -10,6 +11,10 @@ class Bookly extends StatelessWidget {
   const Bookly({super.key});
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(home: SplashView());
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      home: const SplashView(),
+    );
   }
 }
