8093897934a4bf41b202527069dfc17da1b7ec1f
Ahmed Zahran <ahmedzhran82@gmail.com>
2026-01-19 11:34:06 +0200

    add custom list view item to home

--- lib/constans.dart
+++ lib/constans.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF100B20);
 const kLogo = 'assets/images/Logo.png';
+const kTestImage = 'assets/images/half-blood-prince.jpg';
