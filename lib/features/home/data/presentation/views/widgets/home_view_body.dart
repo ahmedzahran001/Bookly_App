d73045d09ee9a5ae2172725e5df8cba270da0140
Ahmed Zahran <ahmedzhran82@gmail.com>
2026-01-19 19:59:47 +0200

    best seller list view item part 1

--- lib/features/home/data/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/data/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/constans.dart';
 import 'package:bookly/core/styles.dart';
 import 'package:bookly/features/home/data/presentation/views/widgets/books_list_view.dart';
 import 'package:bookly/features/home/data/presentation/views/widgets/custom_app_bar.dart';
@@ -17,7 +18,38 @@ class HomeViewBody extends StatelessWidget {
           padding: const EdgeInsets.only(left: 16, top: 32),
           child: Text('Best Seller', style: Styles.titleMedium),
         ),
+        BestSellerListViewItem(),
       ],
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, top: 8),
+      child: SizedBox(
+        height: 140,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.7 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(kTestImage),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
