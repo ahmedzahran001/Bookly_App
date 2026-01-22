97cd3078539be03907b3c4ec0134a7f664589f33
Ahmed Zahran <ahmedzhran82@gmail.com>
2026-01-22 14:11:56 +0200

    create home repo methods & failure file

---
+++ lib/core/errors/failures.dart
@@ -0,0 +1,11 @@
+abstract class Failures {}
+
+class ServerFailure extends Failures {}
+
+class CacheFailure extends Failures {}
+
+class NetworkFailure extends Failures {}
+
+class UnknownFailure extends Failures {}
+
+ 
