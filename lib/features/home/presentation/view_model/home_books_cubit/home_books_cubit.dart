9b67471996a00ece192068c296c91657eaad9422
Ahmed Zahran <ahmedzhran82@gmail.com>
2026-01-23 11:43:32 +0200

    create home books cubit method ( fetch home books )

--- lib/features/home/presentation/view_model/home_books_cubit/home_books_cubit.dart
+++ lib/features/home/presentation/view_model/home_books_cubit/home_books_cubit.dart
@@ -1,11 +1,25 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 part 'home_books_state.dart';
 
 class HomeBooksCubit extends Cubit<HomeBooksState> {
-  HomeBooksCubit() : super(HomeBooksInitial());
+  HomeBooksCubit(this.homeRepo) : super(HomeBooksInitial());
 
-  
+  final HomeRepo homeRepo;
+
+  Future<void> fetchHomeBooks() async {
+    emit(HomeBooksLoading());
+    var result = await homeRepo.fetchHomeBooks();
+
+    result.fold(
+      (failure) {
+        emit(HomeBooksFailure(failure.errMessage));
+      },
+      (books) {
+        emit(HomeBooksSuccess(books));
+      },
+    );
+  }
 }
