308dc8527fb23665eb2e9ff925c11226c065984d
Ahmed Zahran <ahmedzhran82@gmail.com>
2026-01-30 00:22:34 +0200

    fix HomeBooksListView error

--- lib/features/home/presentation/views/widgets/home_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/home_books_list_view.dart
@@ -2,8 +2,6 @@ import 'package:bookly/core/widgets/animated_loading_indicator.dart';
 import 'package:bookly/core/widgets/animated_no_connection.dart';
 import 'package:bookly/features/home/presentation/view_model/home_books_cubit/home_books_cubit.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_image.dart';
-import 'package:bookly/core/widgets/custom_error_widget.dart';
-import 'package:bookly/features/home/presentation/views/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
